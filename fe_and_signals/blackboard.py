cff118a320c1c2a6541bac20d2d4abd3554dcf15
Andrea Chiaverini <86592351+AndreaChiaverini-Etna@users.noreply.github.com>
2022-10-10 23:35:22 +0200

    Environment Libraries

--- fe_and_signals/blackboard.py
+++ fe_and_signals/blackboard.py
@@ -5,14 +5,16 @@ import yfinance as yf
 from fe import FeatureEngineering
 from risk.risk_signal import RiskSignal
 from signals import Signals
+from models.logit_trades_filter import LogitTrades
 from utils import Utils
+from risk.mkt_fe_config import fe_pars
 
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-# data = yf.Ticker("EURUSD=X").history('5d', interval='1m')['Close']
+data = yf.Ticker("BTC-USD").history('300d', interval='1h')[['Open', 'High', 'Low', 'Close']]
+data.columns = ['open', 'high', 'low', 'close']
 data = pd.read_parquet(os.path.dirname(os.getcwd()) + "/test_data/EURUSD2022.parquet")#.resample('1min').last().dropna()
-data = data.iloc[:5000]
 
 rsi = FeatureEngineering.rsi(data['close'], timeperiod=14)
 s_risk = FeatureEngineering.vola_sma(data['close'], timeperiod=14, on_rets=False)
@@ -24,13 +26,22 @@ data = data.loc[rsi.index]
 signals = Signals.from_rsi(
     data=data,
     rsi=rsi,
-    open_cv=(70, 30),
-    close_cv=(50, 50),
+    open_cv=(75, 25),
+    close_cv=(60, 40),
     s_risk=s_risk,
-    n_std=2,
-    show_graph=False,
+    n_std=3,
+    show_graph=True,
     spread=True,
-    tc_perc=None
+    tc_perc=None, #0.002
 )
 
-trade_history = RiskSignal.get_trade_history(s_signals=signals, data=data, spread=True)
+trade_history = RiskSignal.get_trade_history(s_signals=signals, data=data, spread=True, real_start_end=False)  #, tc_perc=0.002)
+
+mkt_fe = RiskSignal.get_mkt_features_by_trades(
+    trades_start_dates=trade_history['start'].to_list(),
+    data=data,
+    fe_pars=fe_pars,
+)
+mkt_fe.to_parquet(r"C:\Users\andre\Dropbox\Horus\test_data\mkt_fe_for_logit_EURUSD.parquet")
+trade_history.to_pickle(r"C:\Users\andre\Dropbox\Horus\test_data\rsi_trades_history_EURUSD.pkl")
+
