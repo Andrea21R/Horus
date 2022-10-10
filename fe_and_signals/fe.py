91a516d9c099a3220843e316c1a54f62d8004bae
Andrea Chiaverini <86592351+AndreaChiaverini-Etna@users.noreply.github.com>
2022-10-10 23:30:13 +0200

    Fe: volatility & mkt_fe

--- fe_and_signals/fe.py
+++ fe_and_signals/fe.py
@@ -1,9 +1,16 @@
 import pandas as pd
 import numpy as np
 import talib as ta
+import hurst
+from typing import Tuple
 
 import aot_module as aot
 from fe_and_signals.utils import Utils
+
+"""
+Migliorie & Commenti:
+- velocizzare Hurst (troppo lento ora)
+"""
 
 
 class FeatureEngineering(object):
@@ -135,3 +142,83 @@ class FeatureEngineering(object):
             ema[timeperiod - 1] = tmp_ema[-1]
 
         return pd.Series(s_arr, index=s.index)
+
+    @staticmethod
+    def atr(data: pd.DataFrame, timeperiod: int) -> pd.Series:
+        """
+        Returns the Average-True-Range (ATR)
+        :param data: pd.DataFrame, with at least columns: ['high', 'low', 'close']
+        :param timeperiod: int, window
+        :return: pd.Series
+        """
+        Utils.check_min_obs(data, min_len=timeperiod + 1)
+        return ta.ATR(data['high'], data['low'], data['close'], timeperiod=timeperiod)
+
+    @staticmethod
+    def bars_dispersion(data: pd.DataFrame) -> pd.Series:
+        """
+        It returns the dispersion of the bars. It's computed as follow:
+            (HIGH - LOW) / LOW
+        Thus, it's bounded between 0 and 1 --> [0, 1]
+        :param data: pd.DataFrame, with at least the columns: ['high', 'low']
+        :return: pd.Series
+        """
+        return (data['high'] - data['low']) / data['low']
+
+    @classmethod
+    def bars_dispersion_rolling(cls, data: pd.DataFrame, timeperiod: int) -> pd.Series:
+        """
+        Returns SMA of the dispersion of the bars. See FeaturesEngineering.bars_dispersion func's docs for more details.
+        :param data: pd.DataFrame, with at least the columns: ['high', 'low']
+        :timeperiod: int, window for SMA
+        :return: pd.Series
+        """
+        Utils.check_min_obs(data, min_len=timeperiod)
+
+        bars_dispersion =  cls.bars_dispersion(data)
+        if timeperiod == 1:
+            return bars_dispersion
+        else:
+            return cls.sma(bars_dispersion, timeperiod=timeperiod)
+
+    @staticmethod
+    def hurst_exp(s_price: pd.DataFrame, timeperiod: int) -> pd.Series:
+        """
+        Returns Hurst Exponent Indicator. It detects if the series shows persistent pattern (i.e. trend period) or
+        anti-persistent one (i.e. mean-reverting period). For further references see:
+            - https://towardsdatascience.com/introduction-to-the-hurst-exponent-with-code-in-python-4da0414ca52e
+            - https://en.wikipedia.org/wiki/Hurst_exponent
+        For computing Hurst Exponent exist several formulas. This function use the R/S procedure.
+        ----------------------------------------------------------------------------------------------------------------
+        WARNING.1: Due to a constraint from the formula, minimum obs required are 100
+        ----------------------------------------------------------------------------------------------------------------
+        :param s_price: pd.Series, closing_price
+        :param timeperiod: int, window
+        :return: pd.Series
+        """
+        if timeperiod < 100:
+            raise Exception("Due to a Hurst constraint, timeperiod cannot be less then 100")
+        Utils.check_min_obs(s_price, min_len=100 if timeperiod < 100 else timeperiod)
+        return  s_price.rolling(timeperiod).apply(lambda s: hurst.compute_Hc(s, kind='price', simplified=False)[0])
+
+    # ========================================== VOLA ==================================================================
+
+    @classmethod
+    def cross_sma_perc_distance(cls, s_price: pd.Series, lookback: Tuple[int, int]) -> float:
+        """
+        Returns the percentage distance between a shorter SMA and a longest one, computed on the closing prices.
+        ----------------------------------------------------------------------------------------------------------------
+        It's computed as follow:
+                Shorter_SMA / Longer_SMA - 1
+        ----------------------------------------------------------------------------------------------------------------
+        It should be useful to feed some ML models. It's a market features that trying to suggest if we're in a bullish
+        market (shorter > longer) or into a bear market (longer < shorter), and the magnitude of the trend (this is the
+        reason why it returns a distance percentage, instead of a simple dummy {1: bullish; 0: bearish).
+        ----------------------------------------------------------------------------------------------------------------
+        :param data, pd.DataFrame, with at least closing prices (columns named 'close')
+        :param lookback, Tuple[int, int], windows for SMA. First elements will be assigned to the shorter SMA
+        """
+        Utils.check_min_obs(s_price, min_len=lookback[1])
+        short_ma = cls.sma(s_price, timeperiod=lookback[0])
+        long_ma  = cls.sma(s_price,timeperiod=lookback[1])
+        return short_ma / long_ma - 1
